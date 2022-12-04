475d18e388d5c90ece2fe5b1718eeb66703ff3c2
TaeHo Kang <taehokang@TaeHoui-MacBookPro.local>
2022-12-04 14:26:26 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_book_list/screens/detail_screen.dart';
+import 'package:flutter_book_list/screens/list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Book List App',
+      home: ListScreen(),
+    );
+  }
+}
+
